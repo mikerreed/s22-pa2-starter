885d46f17d16db57a06e2b97a5852c26652928b5
Mike Reed <mike@reedtribe.org>
2022-01-19 14:57:22 -0500

    first commit

---
+++ apps/tests_pa2.cpp
@@ -0,0 +1,37 @@
+/**
+ *  Copyright 2021 Mike Reed
+ */
+
+#include "GCanvas.h"
+#include "GBitmap.h"
+#include "GColor.h"
+#include "GPoint.h"
+#include "GRect.h"
+#include "tests.h"
+
+static void test_poly_nodraw(GTestStats* stats) {
+    struct Rec {
+        int count;
+        GPoint pts[4];
+    };
+
+    const Rec polys[] = {
+        { 0, {} },
+        { 1, {} },
+        { 2, {} },
+        // no area
+        { 3, {{0,0}, {3,3}, {1,1}} },
+        { 4, {{0,0}, {3,3}, {0,0}, {3,3}} },
+        // clipped out
+        { 3, {{0,0}, {2,-2}, {3,0}} },
+    };
+    
+    const GPaint paint({1,1,1,1});
+    for (const auto& p : polys) {
+        GBitmap bm;
+        bm.alloc(3, 3);
+        auto canvas = GCreateCanvas(bm);
+        canvas->drawConvexPolygon(p.pts, p.count, paint);
+        stats->expectTrue(expect_pixels_value(bm, 0), "rect should not draw");
+    }
+}
